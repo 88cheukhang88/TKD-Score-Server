5695aa2399bd5246df3b1b2b3f8320656130ac48
Mick Crozier <mick@backstagesolutions.com.au>
2015-06-23 20:56:25 +1000

    0.5.0 real

--- api/models/Mat.js
+++ api/models/Mat.js
@@ -228,7 +228,7 @@ module.exports = {
 				}
 			});
 
-			log.mat(this.toString() = ': Registering Judge ' + identifier + ' into slot ' + judge);
+			log.mat(this.toString() + ': Registering Judge ' + identifier + ' into slot ' + judge);
 			
 			if(judge !== false) {
 			
